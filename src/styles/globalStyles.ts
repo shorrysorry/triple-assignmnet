781f6004a7d389f23a178629d249e48c1ac00215
김수빈 <ksb334ksb@gmail.com>
2022-06-26 23:11:23 +0900

    Add #5 - globalStyle 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,1 +1,16 @@
-export function GlobalStyles() {}
+import { createGlobalStyle } from "styled-components";
+import styledReset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${styledReset}
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    font-family: 'Courier New', Courier, monospace;
+    }
+
+`;
+
+export default GlobalStyle;
