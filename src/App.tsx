b2101557a27f00b6ff4647d2af4110a8765b1bfb
김수빈 <ksb334ksb@gmail.com>
2022-06-26 23:37:30 +0900

    Add #6 - theme 세팅 추가
    Add #6 - theme provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/globalStyles";
+import theme from "./styles/theme";
 import Triple from "./pages/Triple";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Triple />} />
         </Routes>
       </BrowserRouter>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 }
 
