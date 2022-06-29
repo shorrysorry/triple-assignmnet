53ee6a2b38c05245e0c645099a3b06fb7c4ab0db
김수빈 <ksb334ksb@gmail.com>
2022-06-29 18:42:56 +0900

    Add #7 - RecordSection 레이아웃 및 에니메이션 구현

--- src/components/organisms/organism.tsx
+++
@@ -1,3 +0,0 @@
-export function Organism() {
-  return <div>hello</div>;
-}
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+
+import RecordSection from "../components/organisms/RecordSection";
+
+const MainWrapperDiv = styled.div`
+  min-width: 1200px;
+`;
+
 export default function Main() {
-  return <div>hello</div>;
+  return (
+    <MainWrapperDiv>
+      <RecordSection />
+    </MainWrapperDiv>
+  );
 }
